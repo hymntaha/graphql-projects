10aed2e6c93a16b624c064cfbaccac6c8823194c
taha uygun <tahauygun@gmail.com>
2019-12-13 22:15:43 -0800

    adding typeDef and resolver for test purpose

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const {ApolloServer} = require('apollo-server-express')
+const { ApolloServer, gql } = require("apollo-server-express");
 
+const typeDefs = gql`
+	type Query {
+		me: User
+	}
+	
+	type User{
+		name: String!
+	}
+`;
 
-const typeDefs = null;
-const resolvers = null;
+const resolvers = {
+	Query:{
+		me: () => {
+			return {
+				name: 'Susan'
+			}
+		}
+	}
+};
 
-
-const server = new ApolloServer ({
-	typeDefs,
-	resolvers
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
 });
 
+server.applyMiddleware({app})
 
-app.list(3333, ()=> console.info('Apollo GraphQL server is running on port 3333'));
+app.list(3333, () =>
+  console.info("Apollo GraphQL server is running on port 3333")
+);
