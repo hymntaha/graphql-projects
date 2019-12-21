3d0cd160b2d21fc6014490d71a1a751f26ec1411
taha uygun <tahauygun@gmail.com>
2019-12-20 23:42:46 -0800

    combining schemas

--- models/index.js
+++ models/index.js
@@ -1,4 +1,12 @@
 const {sequelize} = require('./database');
 
-const UserModel =
-const CarModel =
+const UserModel = sequelize.import('./user');
+const CarModel = sequelize.import('./car');
+
+const models = {
+  User: UserModel,
+  Car: CarModel
+}
+
+
+module.exports = models;
