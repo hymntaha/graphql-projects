3e59924fc279add9440d8007c9d155f16a2f4298
taha uygun <tahauygun@gmail.com>
2019-12-21 23:01:12 -0800

    adding cors in GraphQL

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const { ApolloServer } = require("apollo-server-express");
+const cors = require('cors');
+
+
 const models = require('./models');
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
@@ -17,6 +20,7 @@ const server = new ApolloServer({
 });
 
 server.applyMiddleware({ app });
+app.use(cors());
 
 app.listen(3333, () =>
   console.info("Apollo GraphQL server is running on port 3333")
