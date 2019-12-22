d970e59bb63b0b9404c85ce26daeb8ec6d8c206f
taha uygun <tahauygun@gmail.com>
2019-12-21 22:48:56 -0800

    some more updates

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,37 +1,33 @@
 const resolvers = {
   Query: {
-    users: (parent,args, {models}) => {
+    users: (parent, args, { models }) => {
       return models.User.findAll();
     },
-    user: (parent, { id },{models}) => {
+    user: (parent, { id }, { models }) => {
       return models.User.findByPk(id);
-    },
+    }
     // me: (parent, args, {me}) => me
   },
   Mutation: {
-    makeUser: (parent, { id, name },{models}) => {
+    makeUser: (parent, { id, name }, { models }) => {
       const user = {
         name
       };
       return models.User.create(user);
     },
-    removeUser: (parent, { id },{models}) => {
-      let found = false;
-      models.users.filter(user => {
-        if (user.id === id) {
-          found = true;
-        } else {
-          return user;
-        }
-      });
-      if (found) {
-        return true;
-      } else {
-        return false;
-      }
-    },
+    removeUser: (parent, { id }, { models }) => {
+      return models.User.destroy({ where: { id } });
+    }
+  },
   User: {
-    car: (parent,args, {models}) => parent.cars.map(carId => models.cars[carId - 1])
+    car: (parent, args, { models }) => {
+      return models.Car.findAll({
+        where:{
+          userId: parent.id
+        }
+      })
+    }
+
   }
 };
 
