5dee3b2ff35d571cffab8b632383df2582c1d314
taha uygun <tahauygun@gmail.com>
2019-12-21 22:41:22 -0800

    adding some dummy data

--- load-dummy-data.js
+++ load-dummy-data.js
@@ -1,10 +1,50 @@
 const {sequelize} = require('./models/database');
 const models = require('./models');
 
-const createData = () => {
+const createData = async () => {
+  await models.User.create({
+    name: 'Taha',
+    cars: [{
+      make: 'Mercedes',
+      model: 'C300',
+      color: 'black'
+    }]
+  }, {
+    include:[models.Car]
+  })
 
+  await models.User.create({
+    name: 'Susan',
+    cars: [{
+      make: 'Toyota',
+      model: 'Corolla',
+      color: 'white'
+    }]
+  }, {
+    include:[models.Car]
+  })
+
+  await models.User.create({
+    name: 'Steven',
+    cars: [{
+      make: 'Fiat',
+      model: '500',
+      color: 'yellow'
+    },{
+      make: 'Ford',
+      model: 'Focus',
+      color:'Green'
+    }]
+  }, {
+    include:[models.Car]
+  })
 }
 
-sequelize.sync({force:true}).then(()=>{
-  createData()
+sequelize.sync({force:true}).then(async()=>{
+  try{
+    await createData();
+    process.exit()
+  } catch(e) {
+    console.log(e);
+  }
 })
